465a26cb06099153eaf75356bf267c6da67ad7b7
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-02 22:27:06 +1000

    updated  simple header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
   
 
   return (
    <div>
+   <Header/>
     this is our momentum app
    </div>
   )
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header=()=>{
+    return (
+        <div className='container' style={{margin:'0', padding:'0'}}>
+            <div className='header'>
+            <h1 style={{fontSize:'2rem', fontWeight:'bold', FontFamily:'sans-serif', padding:'2px', margin:'8px'}}>Momentum</h1>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
