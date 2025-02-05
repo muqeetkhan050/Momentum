a3ac85686a3ebe5a127ad1c4b2a0b06da13f2b90
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-05 23:23:34 +1000

    New changes

--- src/RightBar.jsx
+++ src/RightBar.jsx
@@ -3,14 +3,12 @@ import react from 'react';
 function RightBar(){
     return (
         <div className='container'>
-            <div className='row' style={{display:'flex', flexDirection:'column',justifyContent:'right',alignItems:'right',backgroundColor:'#0A1931', width:'fit-content',marginLeft:'1200px',marginTop:'-500px',padding:'10px'}}>
-                <h1 style={{color:'white',padding:'10px'}}>this is our right bar</h1>
-                <h2 style={{color:'white',padding:'10px'}}>this is our right bar</h2>
-                <h2 style={{color:'white',padding:'10px'}}>this is our right bar</h2>
-                <h2 style={{color:'white',padding:'10px'}}>this is our right bar</h2>
-                <h2 style={{color:'white',padding:'10px'}}>this is our right bar</h2>
-                <h2 style={{color:'white',padding:'10px'}}>this is our right bar</h2>
-
+            <div className='row' style={{display:'flex', flexDirection:'column',justifyContent:'right',alignItems:'right',backgroundColor:'0A1931', width:'fit-content',marginLeft:'1200px',marginTop:'-500px',padding:'10px'}}>
+                <h1 style={{color:'white',padding:'10px'}}>Premium </h1>
+                <h2 style={{color:'white',padding:'10px'}}>Communities</h2>
+                <h2 style={{color:'white',padding:'10px'}}>Features</h2>
+                <h2 style={{color:'white',padding:'10px'}}>Look around</h2>
+                
             </div>
         </div>
 
